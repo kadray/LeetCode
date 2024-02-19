84450505dd8d4114b5710e287ec0ecb31c1cb973
Jan Guz <106522800+kadray@users.noreply.github.com>
2024-02-19 15:05:14 +0100

    Time: 0 ms (100.00%), Space: 41.1 MB (14.30%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        double x=Math.log10(n)/Math.log10(2);
-        boolean result = x == (int) x ? true : false;
-        return result;
+        double x=Math.log10(n)/Math.log10(2); 
+        return x == (int) x ? true : false;
     }
 }
